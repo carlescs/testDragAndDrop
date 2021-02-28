f32128204eccf610821f87312a1c2eeae6b9c881
Carles Company Soler <carlescs@gmail.com>
2021-02-28 08:30:08 +0100

    Add open with button.

--- src/app/components/main-component/main-component.component.ts
+++ src/app/components/main-component/main-component.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-main-component',
   templateUrl: './main-component.component.html',
   styleUrls: ['./main-component.component.scss']
 })
-export class MainComponentComponent implements OnInit {
+export class MainComponentComponent implements OnDestroy {
+  public dragging: boolean;
 
-  constructor() { }
+  public files:File[]=[];
+  private objects=[];
+  public urls:{[index:string]:SafeUrl}={};
 
-  ngOnInit(): void {
+  constructor(private sanitizer:DomSanitizer) { }
+
+  public ngOnDestroy(): void {
+    this.clear();
   }
 
+  public addFile(file: File): void {
+    this.files.push(file);
+    let value: string = URL.createObjectURL(file);
+    this.objects.push(value);
+    this.urls[file.name]=this.sanitizer.bypassSecurityTrustUrl(value);
+  }
+
+  public clear(): void {
+    this.objects.forEach(o=>URL.revokeObjectURL(o));
+    this.objects=[];
+    this.files=[];
+    this.urls={};
+  }
+
+  public addFileNew($event: any): void {
+    for(let file of $event.target.files){
+      this.addFile(file);
+    }
+  }
+
+  public fileDialogOpen(): void {
+    document.querySelectorAll("input[type=file]").forEach((el:HTMLElement)=>el.click());
+  }
 }
--- src/app/directives/drag-and-drop.directive.ts
+++ src/app/directives/drag-and-drop.directive.ts
@@ -1,12 +1,58 @@
-import {Directive, ElementRef} from '@angular/core';
+import {Directive, ElementRef, EventEmitter, Output} from '@angular/core';
 
 @Directive({
   selector: '[appDragAndDrop]'
 })
 export class DragAndDropDirective {
 
-  constructor(private elem:ElementRef) {
-    elem.nativeElement.style="background-color: red;"
+  @Output()
+  public dragging: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  @Output()
+  public fileAdded:EventEmitter<File>=new EventEmitter<File>();
+
+  constructor(private elem: ElementRef) {
+    elem.nativeElement.ondrop = this.onDrop;
+    elem.nativeElement.ondragover = this.onDragOver;
+
+    elem.nativeElement.ondragenter = this.onDragEnter;
+
+    elem.nativeElement.ondragleave = this.onDragLeave;
+    elem.nativeElement.ondragend = this.onDragLeave;
   }
 
+  private onDragLeave = () => {
+    this.dragging.emit(false);
+  };
+
+  private onDragEnter = () => {
+    this.dragging.emit(true);
+  };
+
+  private onDrop = ev => {
+    ev.preventDefault();
+    if (ev.dataTransfer.items) {
+      // Use DataTransferItemList interface to access the file(s)
+      for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+        // If dropped items aren't files, reject them
+        if (ev.dataTransfer.items[i].kind === 'file') {
+          var file = ev.dataTransfer.items[i].getAsFile();
+          this.fileAdded.emit(file);
+          console.log('... file[' + i + '].name = ' + file.name + ', size=' + file.size);
+        }
+      }
+    } else {
+      // Use DataTransfer interface to access the file(s)
+      for (var i = 0; i < ev.dataTransfer.files.length; i++) {
+        console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name + ', size=' + ev.dataTransfer.files[i].size);
+        this.fileAdded.emit(ev.dataTransfer.files[i]);
+      }
+    }
+    this.onDragLeave();
+  };
+
+  private onDragOver = ev => {
+    ev.preventDefault();
+    this.onDragEnter();
+  };
 }
