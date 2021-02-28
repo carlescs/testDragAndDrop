ae27f70482e5723641a668fb3f35f758c1dbe88e
Carles Company Soler <carlescs@gmail.com>
2021-02-28 22:34:35 +0100

    Add analysis of images.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
+import { AppComponent } from './app.component';
 import * as Components from './components';
 import * as Directives from './directives';
 
 @NgModule({
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   declarations: [
     AppComponent,
