9650dbcad7a86519d6a56b29dc334c16244947fe
Carles Company Soler <carlescs@gmail.com>
2021-03-01 18:07:06 +0100

    Switch to table layout.

--- src/app/components/main-component/main-component.component.ts
+++ src/app/components/main-component/main-component.component.ts
@@ -2,6 +2,7 @@ import {Component, OnDestroy} from '@angular/core';
 import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
 import {DescriptionService} from '../../services/description.service';
 import {Subscription} from 'rxjs';
+import {finalize} from 'rxjs/operators';
 
 @Component({
   selector: 'app-main-component',
@@ -11,7 +12,11 @@ import {Subscription} from 'rxjs';
 export class MainComponentComponent implements OnDestroy {
   public dragging: boolean;
 
-  public files: { file: File, description?: string }[] = [];
+  public files: {
+    descriptionLoading: Boolean;
+    file: File,
+    description?: string
+  }[] = [];
   public urls: { [index: string]: SafeUrl } = {};
   private objects = [];
   private subscriptions: Subscription[] = [];
@@ -25,7 +30,7 @@ export class MainComponentComponent implements OnDestroy {
   }
 
   public addFile(file: File): void {
-    this.files.push({file: file});
+    this.files.push({file: file, descriptionLoading: false});
     let value: string = URL.createObjectURL(file);
     this.objects.push(value);
     this.urls[file.name] = this.sanitizer.bypassSecurityTrustUrl(value);
@@ -50,10 +55,15 @@ export class MainComponentComponent implements OnDestroy {
 
   public analyze(): void {
     for (let file of this.files) {
-      if (this.isImage(file.file.type) && file.description==null)
-        this.subscriptions.push(this.descriptionService.getDescription(file.file).subscribe(desc => {
-          file.description = desc.captions[0];
-        }));
+      file.descriptionLoading = true;
+      if (this.isImage(file.file.type) && file.description == null)
+        this.subscriptions.push(this.descriptionService.getDescription(file.file)
+          .subscribe(desc => {
+            file.description = desc.captions[0];
+            file.descriptionLoading=false;
+          },e=>{
+            file.descriptionLoading=false;
+          }));
     }
   }
 
