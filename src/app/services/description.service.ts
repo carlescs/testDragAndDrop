12b18be7c873894a9b3b0cae1abff6468f605c8d
Carles Company Soler <carlescs@gmail.com>
2021-03-04 21:12:24 +0100

    Add progress notification.

--- src/app/services/description.service.ts
+++ src/app/services/description.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpEvent} from '@angular/common/http';
 import {Description} from '../models/description.models';
 import {Observable} from 'rxjs';
 
@@ -10,8 +10,13 @@ export class DescriptionService {
 
   constructor(private http:HttpClient) { }
 
-  public getDescription(file: File):Observable<Description> {
-    return this.http.post<Description>('https://testdnd.azurewebsites.net/api/TestHttp',file,{headers:{'Content-Type':file.type}});
+  public getDescription(file: File): Observable<HttpEvent<Description>> {
+    return this.http.post<Description>('https://testdnd.azurewebsites.net/api/TestHttp',file,
+      {
+        headers:{'Content-Type':file.type},
+        reportProgress: true,
+        observe: 'events'
+      });
   }
 
 }
